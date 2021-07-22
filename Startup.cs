c0af3233649f3e7c8a65fae7ef6a6aea59661cee
Lucas-Lameira <lucasslameira@gmail.com>
2021-07-22 20:02:23 -0300

    scoped service

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,8 @@ namespace apidotnet
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            
+            services.addScoped<Data.MongoDB>()
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
